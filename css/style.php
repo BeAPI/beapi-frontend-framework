ab8703a22a89fd3cb9c135770d388f0be6584e5a
nicolas <nicolas@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2011-11-07 09:44:43 +0000

    add main element make adapt.js work with less concept

--- css/style.php
+++ css/style.php
@@ -2,7 +2,7 @@
 header("Content-Type: text/css"); 
 require 'lessc.inc.php';
 
-foreach( array('input.less') as $file ) {
+foreach( array('reset.css', 'text.css', 'master.css') as $file ) {
 	$less = new lessc( dirname(__FILE__) . '/'.$file);
 	echo $less->parse();
 }

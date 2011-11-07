7918825293042cd48985d623d5e1984e360e4ef6
nicolas <nicolas@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2011-11-07 10:27:14 +0000

    add master.less for developing main webside design

--- css/style.php
+++ css/style.php
@@ -2,7 +2,7 @@
 header("Content-Type: text/css"); 
 require 'lessc.inc.php';
 
-foreach( array('reset.css', 'text.css', 'master.css') as $file ) {
+foreach( array('reset.css', 'text.css', 'master.less') as $file ) {
 	$less = new lessc( dirname(__FILE__) . '/'.$file);
 	echo $less->parse();
 }

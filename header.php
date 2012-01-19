5cf63977e1407cfa947644844da2a81787f60679
msimeon <msimeon@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2012-01-19 12:06:28 +0000

    Start integration HTML & CSS

---
+++ header.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="fr"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="fr"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="fr"> <![endif]-->
+<!--[if IE 9 ]> <html class="no-js ie9" lang="fr"> <![endif]-->
+<!--[if !(IE)]><!-->
+<html class="no-js" lang="fr"> <!--<![endif]-->
+	<head profile="http://gmpg.org/xfn/11">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title>IP France</title>
+		
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="apple-touch-icon" href="apple-touch-icon.png">
+		
+		<link rel="stylesheet" href="style.css" />
+		
+		<script type="text/javascript">
+			//<![CDATA[
+			(function(){
+				var c = document.documentElement.className;
+				c = c.replace(/no-js/, 'js');
+				document.documentElement.className = c;
+			})();
+			//]]>
+		</script>
+	</head>
+	
+	<body>
+		<div id="header">
+			<div class="wrapper">
+				
+				<h1 id="logo"><a href="#" class="current"><img src="uploads/logo.png" alt="IP France" width="88" height="56" /><span></span></a></h1>
+				<div class="content-header">
+					<div class="radio">
+						<div class="col280">
+							<img src="uploads/cover-1.png" alt="cover-1" width="50" height="50" class="cover" />
+							<p>
+								Ca peut vous arriver
+								<span class="autor">Julien Courbet</span>
+							</p>
+							<div class="play">
+								<img src="uploads/logo-rtl.png" alt="logo-rtl" width="28" height="22" class="logo" />
+								<a href="" class="player">Ecouter</a>
+							</div>
+						</div>
+						<div class="col280">
+							<p>
+								<span class="autor">Red hot chili peppers</span>
+								> Where the streets have ...
+							</p>
+							<div class="play">
+								<img src="uploads/logo-rtl2.png" class="logo" alt="logo-rtl2" width="23" height="24" />
+								<a href="" class="player">Ecouter</a>
+							</div>
+						</div>
+						<div class="col280">
+							<img src="uploads/cover-2.png" alt="cover-2" width="50" height="50" class="cover" />
+							<p>
+								<span class="autor">Charlie Winston</span>
+								> In the pit of the stomach
+							</p>
+							<div class="play">
+								<img src="uploads/logo-funradio.png" class="logo" alt="logo-funradio" width="52" height="19" />
+								<a href="" class="player">Ecouter</a>
+							</div>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<ul class="nav">
+						<li class="rtl"><a href="#">RTL<span></span></a></li>
+						<li class="rtl2"><a href="#">RTL2<span></span></a></li>
+						<li class="funradio"><a href="#">fun<span></span></a></li>
+						<li class="cgv"><a href="#">CGV & tarifs<span></span></a></li>
+						<li class="creation"><a href="#">Création<span></span></a></li>
+						<li class="ipregions"><a href="#">IP régions<span></span></a></li>
+						<li class="espaceclient"><a href="#">Espace client<span></span></a></li>
+						<li class="nousrejoindre"><a href="#">Nous rejoindre<span></span></a></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div><!-- /#header -->
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+	include 'header.php';
+?>
+
+<?php
+	include 'footer.php';
+?>

7f6c862194d2ace275c1e4625812776e12522a82
nicolas <nicolas@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2012-01-30 09:46:24 +0000

    remove first file of msimeon project

--- footer.php
+++
@@ -1,7 +0,0 @@
-	</body>
-	
-	<!-- Javascript: start with jquery -->
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
-	<script type="text/javascript" src="js/scripts.js"></script>
-	
-</html>
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include 'header.php';
-?>
-
-<?php
-	include 'footer.php';
-?>

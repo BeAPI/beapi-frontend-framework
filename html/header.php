5c0cb0cfe817d74d6ad64b62126458b6476e4980
admin <admin@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2012-08-02 13:06:04 +0000

    move icons on folder, improve header order elements, conditional PIE by default

--- html/header.php
+++ html/header.php
@@ -4,15 +4,36 @@
 <!--[if IE 8 ]>    <html class="no-js ie8 modern-ie" lang="fr"> <![endif]-->
 <!--[if IE 9 ]> <html class="no-js ie9" lang="fr"> <![endif]-->
 <!--[if !(IE)]><!-->
-<html class="no-js" lang="fr"> <!--<![endif]-->
+<html class="no-js" lang="fr"><!--<![endif]-->
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+		<script type="text/javascript">
+			//<![CDATA[
+			(function(){
+				var c = document.documentElement.className;
+				c = c.replace(/no-js/, 'js');
+				document.documentElement.className = c;
+			})();
+			//]]>
+		</script>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 		
 		<title>BeAPI Base theme</title>
 		
-		<link rel="shortcut icon" href="favicon.ico">
-		<link rel="apple-touch-icon" href="apple-touch-icon.png">
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
+		
+		<!--[if lt IE 9]><script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		<!--[if lte IE 8]>
+			<style type="text/css" media="screen">
+				/*
+				.css3-fix {
+					behavior: url(<?php echo 'http://'.$_SERVER['SERVER_NAME']; ?>/wp-content/html/htc/PIE.php);
+				}
+				*/
+			</style>
+		<![endif]-->
 		
 		<link rel="stylesheet" href="css/style.php" />
 		
@@ -26,26 +47,6 @@
 		<!-- Superfish -->
 		<script type="text/javascript" src="js/ressources/hoverIntent.js"></script>
 		<script type="text/javascript" src="js/ressources/superfish.js"></script>
-		
-		<script type="text/javascript">
-			//<![CDATA[
-			(function(){
-				var c = document.documentElement.className;
-				c = c.replace(/no-js/, 'js');
-				document.documentElement.className = c;
-			})();
-			//]]>
-		</script>
-		
-		<!--[if lt IE 9]><script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-		<!--[if lte IE 8]>
-			<style type="text/css" media="screen">
-				.css3-fix {
-					behavior: url(<?php echo 'http://'.$_SERVER['SERVER_NAME']; ?>/wp-content/html/htc/PIE.php);
-				}
-			</style>
-		<![endif]-->
-		
 	</head>
 	<body class="<?php echo $class; ?>">
 		<div class="wrapper">

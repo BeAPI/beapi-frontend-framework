ee79a8442bef7d3619f700e4cdced4d4827b2e51
nicolas <nicolas@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2012-10-09 14:35:04 +0000

    improve widget search page 404

---
+++ html/04-page-404.php
@@ -0,0 +1,31 @@
+<?php $class = 'single'; ?>
+<?php include 'header.php'; ?>
+			<section id="content">
+				<?php include 'blocks/breadcrumb.php'; ?>
+				<div class="single">
+					<h1 class="page-title">Erreur 404</h1>
+					<article class="entry">
+						<section class="entry-content">
+							<p>Désolé mais la page que vous recherchez n'existe plus.</p>
+							<p>Vous pouvez effectuer une recherche :</p>
+							<div class="searchform">
+								<form method="get" action="#">
+									<label for="s" class="assistive-text">Recherche</label>
+									<input type="text" class="field" name="s" id="s" placeholder="Recherche">
+									<input type="submit" class="submit" name="submit" id="searchsubmit" value="Recherche">
+								</form>
+							</div>
+						</section>
+					</article>
+				</div>
+			</section>
+			<aside id="sidebar">
+				<div class="widget-area">
+					<?php include 'blocks/widgets/widget-search.php'; ?>
+					<?php include 'blocks/widgets/widget-text.php'; ?>
+					<?php include 'blocks/widgets/widget-categories.php'; ?>
+					<?php include 'blocks/widgets/widget-archive.php'; ?>
+					<?php include 'blocks/widgets/widget-pages.php'; ?>
+				</div>
+			</aside>
+<?php include 'footer.php'; ?>

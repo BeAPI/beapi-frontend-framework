a33dc27c1dd1dbb239a3c560dc9e192114280fd0
nicolas <nicolas@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2012-11-29 13:32:39 +0000

    jquery 1.8

--- html/header.php
+++ html/header.php
@@ -38,7 +38,7 @@
 		<link rel="stylesheet" type="text/css" media="all" href="css/style.php" />
 		
 		<!-- jQuery -->
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
 		
 		<!-- jQuery UI -->
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>

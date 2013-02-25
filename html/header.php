80a16bfa08f7c395f5327644f3e04a4392f41a2c
nicolas <nicolas@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2013-02-25 08:25:30 +0000

    remove maximum/initial scale for mobile devices

--- html/header.php
+++ html/header.php
@@ -17,7 +17,7 @@
 		</script>
 		
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+		<meta name="viewport" content="width=device-width" />
 		
 		<title>BeAPI Base theme</title>
 		

ecf371bfc7d7a9ef453ebfd9ddcad673bcc55cdb
bbertout <bbertout@22e3aa61-1726-4c8d-b8df-93e6bc5b179e>
2013-09-19 12:48:13 +0000

    fix viewport (initial-scale=1.0)

--- html/header.php
+++ html/header.php
@@ -8,7 +8,7 @@
 	<head>
 		
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<meta name="viewport" content="width=device-width" />
+		<meta name="viewport" content="initial-scale=1.0" />
 		
 		<title>BeAPI Base theme</title>
 		

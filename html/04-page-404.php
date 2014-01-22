1a2e3ec269b7fffcaf0be37a58b9279060759dde
Nicolas Fiascaro <n.fiascaro@gmail.com>
2014-01-22 12:20:49 +0100

    move native searchform in a block

--- html/04-page-404.php
+++ html/04-page-404.php
@@ -7,13 +7,9 @@
 					<section class="entry-content">
 						<p>Désolé mais la page que vous recherchez n'existe plus.</p>
 						<p>Vous pouvez effectuer une recherche :</p>
-						<div class="searchform">
-							<form method="get" action="#">
-								<label for="s" class="assistive-text">Recherche</label>
-								<input type="text" class="field" name="s" id="s" placeholder="Recherche">
-								<input type="submit" class="submit" name="submit" id="searchsubmit" value="Recherche">
-							</form>
-						</div>
+						
+						<?php include 'searchform.php'; ?>
+
 					</section>
 				</article>
 			</section>
---
+++ html/searchform.php
@@ -0,0 +1,7 @@
+<div class="searchform">
+	<form method="get" action="#">
+		<label for="s" class="assistive-text">Recherche</label>
+		<input type="text" class="field" name="s" id="s" placeholder="Recherche">
+		<input type="submit" class="submit" name="submit" id="searchsubmit" value="Recherche">
+	</form>
+</div>

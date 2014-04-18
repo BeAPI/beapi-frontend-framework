59dd094671cff3457243edda074369b74e1e97b4
Amaury Balmer <amaury@beapi.fr>
2014-04-18 09:20:00 +0200

    PHPcomment / Refactoring

--- functions/bea-images.php
+++ functions/bea-images.php
@@ -7,16 +7,24 @@ class BEA_Images{
 	private static $image_sizes;
 	private static $hooks;
 
+	/**
+	 * Construct
+	 */
 	public function __construct() {
+		// Get data from JSON
 		self::load_image_sizes();
 		self::load_locations();
 		self::load_hooks();
+		
+		// Set image size to WP
 		self::add_image_sizes();
 		
+		// Hook WP function for add new attribute
 		add_filter('wp_get_attachment_image_attributes', array(__CLASS__, 'get_attributes'),10,2 );
 	}
 	/*
 	 * Load JSON Image Sizes
+	 * 
 	 * @author Alexandre Sadowski
 	 */
 	public static function load_image_sizes(){
@@ -24,21 +32,54 @@ class BEA_Images{
 			return false;
 		}
 		
-		$json_content = file_get_contents( BEA_IMAGES_JSON_DIR.'image-sizes.json' );
-		$result = json_decode( $json_content );
-		if( !is_array( $result ) || empty( $result ) ){
+		$file_content = file_get_contents( BEA_IMAGES_JSON_DIR.'image-sizes.json' );
+		$result = json_decode( $file_content );
+		if( is_array( $result ) && !empty( $result ) ){
+			self::$image_sizes = $result;
+		}
+	}
+	
+	/*
+	 * Load locations JSON
+	 * 
+	 * @author Alexandre Sadowski
+	 */
+	public static function load_locations(  ){
+		if( !is_file( BEA_IMAGES_JSON_DIR.'image-locations.json' ) ){
 			return false;
 		}
 		
-		self::$image_sizes = $result;
+		$file_content = file_get_contents( BEA_IMAGES_JSON_DIR.'image-locations.json' );
+		$result = json_decode( $file_content );
+		if( is_array( $result ) && !empty( $result ) ){
+			self::$locations = $result;
+		}
+	}
+	
+	/*
+	 * Load hooks JSON
+	 * 
+	 * @author Alexandre Sadowski
+	 */
+	public static function load_hooks(  ){
+		if( !is_file( BEA_IMAGES_JSON_DIR.'image-hooks.json' ) ){
+			return false;
+		}
+		
+		$file_content = file_get_contents( BEA_IMAGES_JSON_DIR.'image-hooks.json' );
+		$result = json_decode( $file_content );
+		if( is_array( $result ) && !empty( $result ) ){
+			self::$hooks = $result;
+		}
 	}
 	
 	/*
 	 * Add Image Sizes in WP
+	 * 
 	 * @author Alexandre Sadowski
 	 */
-	public static function add_image_sizes(  ){
-		if( !is_array(self::$image_sizes) | empty(self::$image_sizes) ){
+	public static function add_image_sizes() {
+		if( !is_array(self::$image_sizes) || empty(self::$image_sizes) ) {
 			return false;
 		}
 		
@@ -47,49 +88,14 @@ class BEA_Images{
 				if( empty($attributes) ){
 					continue;
 				}
+				
 				if( isset($attributes->width) && !empty( $attributes->width ) && isset($attributes->height) && !empty( $attributes->height ) && isset($attributes->crop) ){
 					add_image_size($name, $attributes->width, $attributes->height, $attributes->crop);
 				}
 			}
 		}
-	}
-	/*
-	 * Get attributes of an image size
-	 * 
-	 * @value string $location The location name used in JSON
-	 * @return array|false $attributes Array of attributes in JSON : width, height, crop
-	 * 
-	 * @author Alexandre Sadowski
-	 */
-	public static function get_image_size( $location = ''){
-		if( !is_array(self::$image_sizes) | empty(self::$image_sizes) ){
-			return false;
-		}
 		
-		foreach ( self::$image_sizes as $key => $value ) {
-			foreach( $value as $name => $attributes ){
-				if( $name == $location ){
-					return $attributes;
-				}
-			}
-		}
-		return false;
-	}
-	
-	/*
-	 * Load locations JSON
-	 * @author Alexandre Sadowski
-	 */
-	public static function load_locations(  ){
-		if( !is_file( BEA_IMAGES_JSON_DIR.'image-locations.json' ) ){
-			return false;
-		}
-		$json_content = file_get_contents( BEA_IMAGES_JSON_DIR.'image-locations.json' );
-		$result = json_decode( $json_content );
-		if( !is_array( $result ) || empty( $result ) ){
-			return false;
-		}
-		self::$locations = $result;
+		return true;
 	}
 	
 	/*
@@ -112,23 +118,32 @@ class BEA_Images{
 				}
 			}
 		}
+		
 		return false;
 	}
 	
+	
 	/*
-	 * Load hooks JSON
+	 * Get attributes of an image size
+	 * 
+	 * @value string $location The location name used in JSON
+	 * @return array|false $attributes Array of attributes in JSON : width, height, crop
+	 * 
 	 * @author Alexandre Sadowski
 	 */
-	public static function load_hooks(  ){
-		if( !is_file( BEA_IMAGES_JSON_DIR.'image-hooks.json' ) ){
+	public static function get_image_size( $location = ''){
+		if( !is_array(self::$image_sizes) | empty(self::$image_sizes) ){
 			return false;
 		}
-		$json_content = file_get_contents( BEA_IMAGES_JSON_DIR.'image-hooks.json' );
-		$result = json_decode( $json_content );
-		if( !is_array( $result ) || empty( $result ) ){
-			return false;
+		
+		foreach ( self::$image_sizes as $key => $value ) {
+			foreach( $value as $name => $attributes ){
+				if( $name == $location ){
+					return $attributes;
+				}
+			}
 		}
-		self::$hooks = $result;
+		return false;
 	}
 	
 	/*
@@ -172,27 +187,28 @@ class BEA_Images{
 		$location_array = self::get_location( $args['location'] );
 		if( empty( $location_array ) ){
 			$args['data-location'] = 'No location found';
-		}
-		$srcset_attrs = array();
-		foreach( $location_array as $location ){
-			if( !isset( $location->size ) || empty( $location->size ) ){
-				continue;
-			}
-			
-			$img = wp_get_attachment_image_src( $attachment->ID, $location->size );
-			if( empty($img) ){
-				continue;
-			}
-			
-			if( isset( $location->class ) && !empty( $location->class ) ){
-				$args['class'] = $args['class']. ' '.$location->class;
-			}
+		} else {
+			$srcset_attrs = array();
+			foreach( $location_array as $location ){
+				if( !isset( $location->size ) || empty( $location->size ) ){
+					continue;
+				}
 
-			$srcset_attrs[] = $img[0].' '.$location->srcset;
-		}
+				$img = wp_get_attachment_image_src( $attachment->ID, $location->size );
+				if( empty($img) ){
+					continue;
+				}
+
+				if( isset( $location->class ) && !empty( $location->class ) ){
+					$args['class'] = $args['class']. ' '.$location->class;
+				}
+
+				$srcset_attrs[] = $img[0].' '.$location->srcset;
+			}
 		
-		if( !empty($srcset_attrs) ){
-			$args['srcset'] = implode(', ', $srcset_attrs);
+			if( !empty($srcset_attrs) ){
+				$args['srcset'] = implode(', ', $srcset_attrs);
+			}
 		}
 		
 		return $args;

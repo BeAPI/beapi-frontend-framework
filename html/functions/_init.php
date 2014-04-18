274d812a9c0baaab7cfbe38a34261eef396e9298
Amaury Balmer <amaury@beapi.fr>
2014-04-18 09:38:40 +0200

    call new function file

--- html/functions/_init.php
+++ html/functions/_init.php
@@ -3,6 +3,7 @@ define( 'BEA_IMG_SAMPLE_DIR', dirname( __FILE__ ) . '/../../assets/img/sample/' 
 
 require dirname( __FILE__ ) . '/url.php';
 require dirname( __FILE__ ) . '/compat.php';
+require dirname( __FILE__ ) . '/post-thumbnail.php';
 require dirname( __FILE__ ) . '/../functions/bea-images.php';
 
 global $bea_image;

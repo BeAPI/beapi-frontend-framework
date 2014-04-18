6180d266271b670e7a6c8ca2cfb9e0c52b460754
Amaury Balmer <amaury@beapi.fr>
2014-04-18 08:50:01 +0200

    fix path to bea image classes

--- html/functions/_init.php
+++ html/functions/_init.php
@@ -3,7 +3,7 @@ define( 'BEA_IMG_SAMPLE_DIR', dirname( __FILE__ ) . '/../../assets/img/sample/' 
 
 require dirname( __FILE__ ) . '/url.php';
 require dirname( __FILE__ ) . '/compat.php';
-require dirname( __FILE__ ) . '/../bea-images.php';
+require dirname( __FILE__ ) . '/../functions/bea-images.php';
 
 global $bea_image;
 $bea_image = new BEA_Images();

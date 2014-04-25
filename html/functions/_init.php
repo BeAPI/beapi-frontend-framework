0dac579fdc38b68ca0b50a5d5f76cd13da34f65c
Alexandre Sadowski <asadowski@beapi.fr>
2014-04-25 10:19:04 +0200

    fix require path for class bea images

--- html/functions/_init.php
+++ html/functions/_init.php
@@ -4,7 +4,7 @@ define( 'BEA_IMG_SAMPLE_DIR', dirname( __FILE__ ) . '/../../assets/img/sample/' 
 require dirname( __FILE__ ) . '/url.php';
 require dirname( __FILE__ ) . '/compat.php';
 require dirname( __FILE__ ) . '/post-thumbnail.php';
-require dirname( __FILE__ ) . '/../functions/class-bea-images.php';
+require dirname( __FILE__ ) . '/../../functions/class-bea-images.php';
 
 global $bea_image;
 $bea_image = new BEA_Images();

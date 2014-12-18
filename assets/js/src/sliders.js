3daa8409bee118351c22a955f32709d2a6370cb0
Nicolas Juen <njuen@beapi.fr>
2014-12-18 17:24:55 +0100

    Proposal for the gulp sourcemap generation

---
+++ assets/js/src/sliders.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Nicolas on 18/12/2014.
+ */
+console.log('sliders');
--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,36 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	path = require('path'),
 	minifyCSS = require('gulp-minify-css'),
+	concat = require('gulp-concat-sourcemap'),
 	livereload = require('gulp-livereload');
 
 /*JS task*/
 gulp.task('dev-vendor-js', function () {
 	return gulp.src(['assets/js/vendor/*.min.js', 'assets/js/vendor/*-min.js', 'assets/js/vendor/**/*-min.js'])
-		.pipe(plugins.concat('scripts.dev.js'))
+		.pipe(plugins.concat('vendor.min.js'))
 		.pipe(gulp.dest('assets/js'));
 });
 
 gulp.task('dist-all-js', function () {
-	return gulp.src(['assets/js/vendor/*.min.js', 'assets/js/vendor/*-min.js', 'assets/js/vendor/**/*-min.js', 'assets/js/scripts-domready.js'])
-		.pipe(plugins.jshint())
+	// Make a vendor
+	gulp.src(['assets/js/vendor/*.min.js', 'assets/js/vendor/*-min.js', 'assets/js/vendor/**/*-min.js'])
+		.pipe(plugins.concat('vendor.min.js'))
+		.pipe(gulp.dest('assets/js'));
+
+	// Make the rest
+	return gulp.src(['assets/js/vendor.min.js', 'assets/js/src/*.js'])
 		.pipe(plugins.uglify())
-		.pipe(plugins.concat('scripts.min.js'))
+		.pipe(concat('scripts.min.js', { sourceRoot : '../../' }))
 		.pipe(gulp.dest('assets/js/'));
 });
 
 gulp.task('dev-check-js', function () {
-	return gulp.src('assets/js/scripts-domready.js')
+	// Concat the vendor and the src
+	return gulp.src( [ 'assets/js/vendor.min.js', 'assets/js/src/*.js'])
 		.pipe(plugins.jshint())
 		.pipe(plugins.jshint.reporter('default'))
+		.pipe(plugins.uglify())
+		.pipe(concat('scripts.min.js', { sourceRoot : '../../' }))
 		.pipe(gulp.dest('assets/js/'));
 });
 
@@ -51,7 +60,8 @@ gulp.task('dist-less', function () {
 
 // On default task, just compile on demand
 gulp.task('default', function() {
-	gulp.watch('assets/js/*.js', ['dev-vendor-js', 'dev-check-js']);
+	gulp.watch('assets/js/src/*.js', [ 'dev-check-js']);
+	gulp.watch('assets/js/vendor/*.js', [ 'dev-vendor-js']);
 	gulp.watch(['assets/css/*.less', 'assets/css/**/*.less'], ['dev-less']);
 	livereload.listen();
 	gulp.watch('assets/css/**').on('change', livereload.changed);

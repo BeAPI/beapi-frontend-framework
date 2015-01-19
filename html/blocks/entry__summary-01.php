bbfb9fb45f7ebecd471fb7743f732f4f0a9e32f7
cocola <nfiascaro@beapi.fr>
2015-01-19 17:53:07 +0100

    maj picturefill 2.0.0 to 2.2.0

--- html/blocks/entry__summary-01.php
+++ html/blocks/entry__summary-01.php
@@ -1,7 +1,9 @@
 <!-- Entry summary sample for loop -->
 <article class="entry__summary">
 	<a class="alignleft" href="#">
-		<img src="http://placehold.it/150x150" width="150" height="150" alt="#">
+		<!-- Local dev only
+		<img src="http://placehold.it/150x150" width="150" height="150" alt="#"> -->
+		<?php echo get_the_post_thumbnail( 0, 'thumbnail', array( 'data-location' => 'entry-img-01' ) ); ?>
 	</a>
 	<h2 class="entry__title">
 		<a href="#">There is a post + a 20 words lorem ipsum excerpt</a>

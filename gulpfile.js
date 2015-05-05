c979c4c5ec561b6c4d6c0d03a4b2a220889e4f04
cocola <nfiascaro@beapi.fr>
2015-05-05 12:44:44 +0200

    px to rem everyting

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,8 @@ var gulp = require('gulp'),
 	reload = browserSync.reload;
 
 var pxtoremOptions = {
-	replace: false
+	replace: false,
+	prop_white_list: ['font', 'font-size', 'line-height', 'letter-spacing', 'margin', 'padding', 'border', 'border-top', 'border-left', 'border-bottom', 'border-right', 'border-radius', 'width', 'height', 'top', 'left', 'bottom', 'right']
 };
 
 /*Set server*/

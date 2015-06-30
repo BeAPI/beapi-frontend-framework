50f9c9d9193983a2ddb9b72f396e38b266742e07
cocola <nfiascaro@beapi.fr>
2015-06-30 15:49:42 +0200

    add comment

--- html/blocks/entry__summary-01.php
+++ html/blocks/entry__summary-01.php
@@ -5,6 +5,7 @@
 		<img src="http://placehold.it/150x150" width="150" height="150" alt="#">
 		<!-- end Local dev only -->
 		<!-- <?php echo get_the_post_thumbnail( 0, 'thumbnail', array( 'data-location' => 'entry-img-01' ) ); ?> -->
+		<!-- thumbnail to be displayed in google search result -->
 		<meta itemprop="thumbnailUrl" content="http://placehold.it/150x150">
 	</a>
 	<h2 class="entry__title" itemprop="headline">

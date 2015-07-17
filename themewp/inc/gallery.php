ad9375e33774400fe1f8d86326ac993f7830d196
cocola <nfiascaro@beapi.fr>
2015-07-17 16:06:42 +0200

    remove themewp folder

--- themewp/inc/gallery.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Remove inline styles printed when the gallery shortcode is used.
- *
- * Galleries are styled by the theme in style.css. This is just
- * a simple filter call that tells WordPress to not use the default styles.
- *
- */
-add_filter( 'use_default_gallery_style', '__return_false' );

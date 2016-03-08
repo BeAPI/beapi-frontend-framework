b8ee66b47c0edc96264fdb3a2e770cb15f93c795
cocola <nfiascaro@beapi.fr>
2016-03-08 12:06:40 +0100

    test lazysise to current resp image implemantation

--- html/footer.php
+++ html/footer.php
@@ -11,6 +11,7 @@
 			// inline loadJS
 			function loadJS(e,t){"use strict";var n=window.document.getElementsByTagName("script")[0],o=window.document.createElement("script");return o.src=e,o.async=!0,n.parentNode.insertBefore(o,n),t&&"function"==typeof t&&(o.onload=t),o}
 			// then load your JS
+			loadJS("../assets/js/vendor_async/lazysizes.min.js");
 			loadJS("../assets/js/scripts.min.js");
 		</script>
 	</body>

644141bca62e9fcdbe699d537f7e55f3e30c711c
mmaniga <mmaniga@beapi.fr>
2016-10-26 10:08:08 +0200

    add loadJs works fine

--- assets/js/src/fonts-css-async.js
+++ assets/js/src/fonts-css-async.js
@@ -1,8 +1,21 @@
-// detect loading of fonts using fontfaceobserver lib
-var roboto = new FontFaceObserver("Roboto");
+// load webfonts asyncusing LoasCSS filament group lib
+loadCSS("//fonts.googleapis.com/css?family=Roboto:400,500,700");
 
-roboto.load().then(function () {
-	document.documentElement.className += " fonts-loaded";
-}).catch(function () {
-  console.log('Font failed to load.');
+// detect loading of fonts using fontfaceobserver lib
+var roboto400 = new FontFaceObserver("Roboto", {
+	weight: 400
 });
+
+roboto400.check().then(function () {
+	document.documentElement.className += " fonts-loaded";
+});
+
+if (typeof Promise === 'function') { // < IE9
+	Promise.all([
+		roboto400.check(),
+	]).then(function() {
+		document.documentElement.className += " fonts-loaded";
+	});
+}else{
+	document.documentElement.className += " fonts-loaded";
+}

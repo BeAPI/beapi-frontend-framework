5fb50caf10a86b12a692cca7b661d2a0e841fa59
Nicolas Fiascaro <nfiascaro@beapi.fr>
2016-10-28 17:56:33 +0200

    Use svg icons instead of font icon. Keep iconfont task for old compatibility projects Reorder and adjust sass files. Add semantic

--- html/footer.php
+++ html/footer.php
@@ -1,12 +1,31 @@
 				</div><!-- main content wrapper -->
 			</main> <!-- Main content -->
-			<footer id="footer">
+			<footer class="footer" id="footer" role="contentInfo">
 				<div class="wrapper">
-					<p>Footer content</p>
+					<p>Follow us:</p>
+					<a class="button button--circle" href="https://www.facebook.com/beapi.agency/?ref=bookmarks">
+						<span class="visuallyhidden">On Facebook</span>
+						<svg class="icon icon-facebook" aria-hidden="true" role="img">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-facebook"></use>
+						</svg>
+					</a>
+					<a class="button button--circle" href="https://twitter.com/be_api">
+						<span class="visuallyhidden">On Twitter</span>
+						<svg class="icon icon-twitter" aria-hidden="true" role="img">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-twitter"></use>
+						</svg>
+					</a>
+					<a class="button button--circle" href="https://www.instagram.com/agencebeapi/">
+						<span class="visuallyhidden">On Instagram</span>
+						<svg class="icon icon-instagram" aria-hidden="true" role="img">
+							<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-instagram"></use>
+						</svg>
+					</a>
+					
 				</div>
 			</footer>
 		</div><!-- Main -->
-		
+		<?php include '../assets/icons/icons.svg'; ?>
 		<script>
 			// inline loadJS
 			function loadJS(e,t){"use strict";var n=window.document.getElementsByTagName("script")[0],o=window.document.createElement("script");return o.src=e,o.async=!0,n.parentNode.insertBefore(o,n),t&&"function"==typeof t&&(o.onload=t),o}

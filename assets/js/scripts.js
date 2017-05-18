46bcf6255e09b3ea5e3f73d6233aea81a39be0aa
Nicolas Fiascaro <nfiascaro@beapi.fr>
2017-05-18 18:09:22 +0200

    remove obsolete tasks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,11 +6,13 @@
 var jQuery = require('jquery');
 
 // Modules
-var menu = require('./src/menu');
+var menu = require('./src/console');
+var menu = require('./src/ie_message');
 var placeholder = require('./src/placeholder');
+var menu = require('./src/menu');
 var lightbox = require('./src/lightbox');
 
 
 jQuery(document).ready(function(){
-	console.log('Init');
+	//console.log('Init');
 });

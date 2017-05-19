86100af83f30b3017cd62559250326a8dd0d4705
Romain Lefort <rlefort@beapi.fr>
2017-05-19 17:20:35 +0200

    require lazysizes and picturefill to scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,21 @@
 /**
  * Main scripts file
+ *
  */
 
 // Dependencies
 var jQuery = require('jquery');
 
+// Vendor
+var lazysizes = require('./vendor/lazysizes');
+var picturefill = require('./vendor/picturefill');
+
 // Modules
-var menu = require('./src/console');
-var menu = require('./src/ie_message');
+var console = require('./src/console');
+var ieMessage = require('./src/ie_message');
 var placeholder = require('./src/placeholder');
 var menu = require('./src/menu');
 var lightbox = require('./src/lightbox');
-
 
 jQuery(document).ready(function(){
 	//console.log('Init');

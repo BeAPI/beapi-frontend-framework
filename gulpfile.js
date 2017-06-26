1c2f92f5bd6677c979e1d9a910163a8013308f45
Romain Lefort <rlefort@beapi.fr>
2017-06-26 11:14:32 +0200

    Remove js-vendor from gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -36,14 +36,14 @@ gulp.task('bs-reload', getTask('bs-reload'));
 // On default task, just compile on demand
 gulp.task('default', ['js', 'sass', 'svgicons'], function() {
 	gulp.watch('assets/js/src/*.js', [ 'js' ]);
-	gulp.watch('assets/js/vendor/*.js', [ 'js-vendor', 'js' ]);
+	gulp.watch('assets/js/vendor/*.js', [ 'js' ]);
 	gulp.watch(['assets/css/*.scss', 'assets/css/**/*.scss'], ['sass']);
 	gulp.watch(['assets/img/icons/*.svg'], ['svgicons', 'sass']);
 });
 // Browser sync with local setup.
 gulp.task('serve', ['browser-sync', 'server', 'bs-reload', 'js', 'sass', 'svgicons'], function() {
 	gulp.watch('assets/js/src/*.js', [ 'js' ]);
-	gulp.watch('assets/js/vendor/*.js', [ 'js-vendor', 'js' ]);
+	gulp.watch('assets/js/vendor/*.js', [ 'js' ]);
 	gulp.watch(['assets/css/*.scss', 'assets/css/**/*.scss'], ['sass']);
 	gulp.watch(['assets/img/icons/*.svg'], ['svgicons', 'sass']);
 	gulp.watch(['html/**/*.php', 'assets/css/style.css', 'assets/js/scripts.min.js'], ['bs-reload']);

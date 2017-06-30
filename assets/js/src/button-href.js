66243e5040a3577ca34547334622be996babeda7
Romain Lefort <rlefort@beapi.fr>
2017-06-30 11:08:55 +0200

    Button href add force download attributes

--- assets/js/src/button-href.js
+++ assets/js/src/button-href.js
@@ -14,11 +14,19 @@ $(document).keyup(function(){
 // Handle data-href on button components
 $('body').on('mousedown', '[data-href]', function (e) {
 	var href = $(this).data('href');
-	var isBlank = $(this).data('blank');
-	console.log(e.which);
+	var isBlank = $(this).attr('data-target', '_blank');
+	var download = $(this).attr('data-target', 'download');
+	var filename = $(this).data('filename');
 	if (isBlank || e.which === 2 || cntrlIsPressed) {
 		window.open(href, '_blank');
 	} else if (e.which === 1) {
 		window.location.href = href;
+	}
+	if (download) {
+		var anchor = document.createElement('a');
+		anchor.href = href;
+		anchor.target = '_blank';
+		anchor.download = filename;
+		anchor.click();
 	}
 });

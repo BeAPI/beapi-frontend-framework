8d9ae2ed17b493384586d89e4ad348bcabac8941
Milan Ricoul <mricoul@beapi.fr>
2017-08-17 23:56:12 +0200

    Clean app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,16 +9,12 @@ const jQuery 			= require('jquery');
 // Vendor
 const lazysizes 		= require('lazysizes');
 const lazysizesBgset 	= require('./vendor/lazysizes.bgset');
-const picturefill 		= require('./vendor/picturefill');
+const picturefill 		= require('picturefill');
 
 // Modules
+const buttonHref 		= require('./src/button-href');		
 const consoleLog 		= require('./src/console');
 const ieMessage 		= require('./src/ie_message');
-const placeholder 		= require('./src/placeholder');
 const menu 				= require('./src/menu');
 const lightbox 			= require('./src/lightbox');
-const buttonHref 		= require('./src/button-href');		
-
-jQuery(document).ready(function(){
-	//console.log('Init');
-});
+const placeholder 		= require('./src/placeholder');

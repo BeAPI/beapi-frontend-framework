d3bd2e1f42920219b2f94fb8b73a4fc2e4fa03d8
alexandreravelli <ravelli.alexandre@gmail.com>
2017-09-06 16:04:01 +0200

    add mixin for checkbox and radio custom

---
+++ html/blocks/radio.php
@@ -0,0 +1,23 @@
+<div class="widget">
+	<h2 class="widgettitle">Radio</h2>
+	<div class="widget__inner">
+		<form action="#">
+			<div class="radio--custom">
+			  <input id="radio-1" name="radio" type="radio" checked>
+			  <label for="radio-1">radio item</label>
+			</div>
+			<div class="radio--custom">
+			<input id="radio-2" name="radio" type="radio">
+			  <label  for="radio-2">radio item</label>
+			</div>
+			<div class="radio--custom">
+			  <input id="radio-3" name="radio" type="radio">
+			  <label for="radio-3">radio item</label>
+			</div>
+			<div class="radio--custom">
+			  <input id="radio-4" name="radio" type="radio">
+			  <label for="radio-4">radio item</label>
+			</div>
+		</form>
+	</div>
+</div>

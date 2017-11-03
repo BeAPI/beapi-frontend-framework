7c25c96382632565c0fa280e19db1a9c897a5fe8
Milan Ricoul <mricoul@beapi.fr>
2017-11-03 14:28:03 +0100

    Webpack config eslint ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,142 +1,140 @@
 /**
  * Dependencies
  */
-const webpack 					= require('webpack'),
-	  path 						= require('path'),
-	  ExtractTextPlugin 		= require('extract-text-webpack-plugin'),
-	  UglifyJsPlugin			= require('uglifyjs-webpack-plugin'),
-	  OptimizeCssAssetsPlugin 	= require('optimize-css-assets-webpack-plugin'),
-	  ManifestPlugin			= require('webpack-manifest-plugin'),
-	  CleanWebpackPlugin		= require('clean-webpack-plugin'),
-	  cssLoaders				= require('./webpack.css-loader.js'),
-	  dev						= process.env.NODE_ENV === 'dev';
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const cssLoaders = require('./webpack.css-loader.js')
+const dev = process.env.NODE_ENV === 'dev'
 
-let root = path.resolve( __dirname );
-
+let root = path.resolve(__dirname)
 
 process.traceDeprecation = true
 
 let config = {
-	entry: {
-		app: ['./assets/css/style.scss', './assets/js/app.js']
-	},
-	output: {
-		path: path.resolve( __dirname, './dist' ),
-		filename: dev ? '[name].js' : '[name].[chunkhash:8].js' 
-	},
-	watch: dev,
-	devtool: dev ? 'source-map' : false,
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				include: root,
-				enforce: 'pre',
-				use: ['eslint-loader']
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [...cssLoaders, 'resolve-url-loader']
-				}),
-			},
-			{
-		      	test: /\.(sass|scss)$/,
-		      	use: ExtractTextPlugin.extract({
-					use: [...cssLoaders, 'sass-loader']
-		      	})
-		    },
-			{
-				test: /\.(woff2?|eot|ttf|otf|mp3|wav)(\?.*)?$/,
-				use: {
-            		loader: 'file-loader',
-            		options: {
-            			name:'[name].[ext]',
-            			outputPath: './fonts/'
-            		}
-            	}
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)$/,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 8192,
-							name: '[name].[ext]',
-							outputPath: './img/'
-						}
-	            	},
-	            	{
-	            		loader: 'img-loader',
-	            		options: {
-	            			enabled: !dev
-	            		}
-	            	}
-            	]
-			}
-		]
-	},
-	plugins: []
+  entry: {
+    app: ['./assets/css/style.scss', './assets/js/app.js']
+  },
+  output: {
+    path: path.resolve( __dirname, './dist' ),
+    filename: dev ? '[name].js' : '[name].[chunkhash:8].js'
+  },
+  watch: dev,
+  devtool: dev ? 'source-map' : false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        include: root,
+        enforce: 'pre',
+        use: ['eslint-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [...cssLoaders, 'resolve-url-loader']
+        })
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: ExtractTextPlugin.extract({
+          use: [...cssLoaders, 'sass-loader']
+        })
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf|mp3|wav)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name:'[name].[ext]',
+            outputPath: './fonts/'
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: './img/'
+            }
+          },
+          {
+            loader: 'img-loader',
+            options: {
+              enabled: !dev
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: []
 }
 
 /**
  * Production
  */
-if(!dev) {
-	/**
-	 * Styles
-	 */
-	
-	config.plugins.push( new ExtractTextPlugin({
-		filename: '[name].[contenthash:8].min.css',
-		allChunks: true,
-	}));
-	
-	config.plugins.push( new OptimizeCssAssetsPlugin({
-	    assetNameRegExp: /\.min\.css$/,
-	    cssProcessorOptions: {
-	    	discardComments: {
-	    		removeAll: true
-	    	}
-	    }
-	}));
-	
-	/**
-	 * Scripts
-	 */
-	config.plugins.push( new UglifyJsPlugin({
-		sourceMap: true,
-		comments: false
-    }));
+if (!dev) {
+  /**
+   * Styles
+   */
 
-	/**
-	 * Assets
-	 * Using on production to load dynamically scripts and style with hashes
-	 */
-    config.plugins.push( new ManifestPlugin({
-    	fileName: 'assets.json'
-	}));
+  config.plugins.push( new ExtractTextPlugin({
+    filename: '[name].[contenthash:8].min.css',
+    allChunks: true
+  }))
 
+  config.plugins.push( new OptimizeCssAssetsPlugin({
+    assetNameRegExp: /\.min\.css$/,
+    cssProcessorOptions: {
+      discardComments: {
+        removeAll: true
+      }
+    }
+  }))
 
-    /**
-     * Clean dist directory before prod build
-     */
-	config.plugins.push( new CleanWebpackPlugin(['dist'], {
-		root: path.resolve('./'),
-		verbose: true,
-		dry: false
-    }));
+  /**
+  * Scripts
+  */
+  config.plugins.push( new UglifyJsPlugin({
+    sourceMap: true,
+    comments: false
+  }))
+
+  /**
+   * Assets
+   * Using on production to load dynamically scripts and style with hashes
+   */
+  config.plugins.push( new ManifestPlugin({
+    fileName: 'assets.json'
+  }))
+
+  /**
+   * Clean dist directory before prod build
+   */
+  config.plugins.push(new CleanWebpackPlugin(['dist'], {
+    root: path.resolve('./'),
+    verbose: true,
+    dry: false
+  }))
 } else {
-	/**
-	 * Styles
-	 */
-	config.plugins.push( new ExtractTextPlugin({
-		filename: '[name].css',
-		allChunks: true,
-	}));
+  /**
+   * Styles
+   */
+  config.plugins.push( new ExtractTextPlugin({
+    filename: '[name].css',
+    allChunks: true
+  }))
 }
 
-module.exports = config;
+module.exports = config

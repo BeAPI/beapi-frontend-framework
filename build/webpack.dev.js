a915e2da412531438d3da19d115c546b2591c19b
Milan Ricoul <mricoul@beapi.fr>
2017-12-02 00:21:45 +0100

    work npm start with refacto

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,9 +1,5 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const webpackBase = require('./webpack.base')
-const server = require('./server')
-const build = process.env.NODE_BUILD === 'true'
-
-let bs = build === 'true' ? server : ''
 
 webpackBase.plugins.push(
   new ExtractTextPlugin({

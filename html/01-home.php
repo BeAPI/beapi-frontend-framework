c5364c3ce3002f27dbf711241bb1550d238e9d60
Nicolas Fiascaro <n.fiascaro@gmail.com>
2018-01-10 10:43:53 +0100

    more explicit instructions with links

--- html/01-home.php
+++ html/01-home.php
@@ -5,8 +5,9 @@
 			<h1 class="entry__title">Welcome to the BFF</h1>
 		</header>
 		<section class="entry__content">
-			<p>This a starter theme so it's empty</p>
-			<p>You can use composerjs in order to add components (html, css or js)</p>
+			<p>This a starter theme so it's empty. <br> Minimum pages are listed in the menu above. It's a base that you have to custom on your need.</p>
+			<p>You can use <a href="https://github.com/BeAPI/beapi-frontend-framework/#composer-js">Composer JS</a> in order to add <a href="https://github.com/BeAPI/bff-components" target="_blank" title="See components">well known components</a> (html, css or js) like comments, widgets, plugins etc.</p>
+			<h2>🦄 Happy Coding ! 🦄</h2>
 		</section>
 	</article>
 

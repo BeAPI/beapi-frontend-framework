a72c937cc3b675c9bb2b64e862aac543a4a4a7b0
Milan Ricoul <mricoul@beapi.fr>
2018-02-02 12:02:01 +0100

    Add appicon task

--- tasks/favicon.js
+++ tasks/favicon.js
@@ -1,8 +1,10 @@
+// https://github.com/evilebottnawi/favicons/
+
 const favicons = require('favicons')
 const fs = require('fs')
 const mkdirp = require('mkdirp')
 const source = 'assets/img/favicons/favicon_src.png'
-const configuration = {
+let configuration = {
   appName: 'BFF',
   appDescription: 'BFF starter theme',
   developerName: 'Be API',
@@ -15,21 +17,45 @@ const configuration = {
   version: 1.0,
   logging: false,
   online: false,
-  html: 'index_hd.html',
   pipeHTML: true,
-  replace: true,
-  icons: {
-    android: true,
-    appleIcon: true,
-    appleStartup: false,
-    coast: true,
-    favicons: false,
-    firefox: true,
-    opengraph: false,
-    windows: true,
-    yandex: true
+  replace: true
+}
+
+if (process.argv.indexOf('--appicon') === -1) {
+  configuration = {
+    ...configuration,
+    html: 'index_sd.html',
+    icons: {
+      android: false,
+      appleIcon: false,
+      appleStartup: false,
+      coast: false,
+      favicons: true,
+      firefox: false,
+      opengraph: false,
+      windows: false,
+      yandex: false
+    }
+  }
+} else {
+  configuration = {
+    ...configuration,
+    html: 'index_hd.html',
+    icons: {
+      android: true,
+      appleIcon: true,
+      appleStartup: false,
+      coast: true,
+      favicons: false,
+      firefox: true,
+      opengraph: false,
+      windows: true,
+      yandex: true
+    }
   }
 }
+
+console.log('Favicons are being generated...')
 
 const callback = function (error, response) {
   if (error) {
@@ -38,24 +64,26 @@ const callback = function (error, response) {
     console.log(error.message) // Error description e.g. 'An unknown error has occurred'
     return
   }
-  console.log(response.images) // Array of { name: string, contents: <buffer> }
-  console.log(response.files) // Array of { name: string, contents: <string> }
-  console.log(response.html) // Array of strings (html elements)
 
   if (response.images) {
     mkdirp.sync(configuration.path)
-    response.images.forEach((image) =>
-      fs.writeFileSync(`${configuration.path}/${image.name}`, image.contents))
+    response.images.forEach((image) => {
+      console.log(`${image.name} has been successfully generated into ${configuration.path}`)
+      return fs.writeFileSync(`${configuration.path}/${image.name}`, image.contents)
+    })
   }
 
   if (response.files) {
     mkdirp.sync(configuration.path)
-    response.files.forEach((file) =>
-      fs.writeFileSync(`${configuration.path}/${file.name}`, file.contents))
+    response.files.forEach((file) => {
+      console.log(`${file.name} has been successfully generated into ${configuration.path}`)
+      return fs.writeFileSync(`${configuration.path}/${file.name}`, file.contents)
+    })
   }
 
   if (response.html) {
-    fs.writeFileSync(`${configuration.path}/test.html`, response.html.join('\n'))
+    console.log(`${configuration.html} is successfully moved to ${configuration.path}`)
+    fs.writeFileSync(`${configuration.path}/${configuration.html}`, response.html.join('\n'))
   }
 }
 

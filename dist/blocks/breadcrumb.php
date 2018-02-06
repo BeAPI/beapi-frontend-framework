01f9731a2e4789de71ff9591df88ebab117bba3a
Milan Ricoul <mricoul@beapi.fr>
2018-02-06 01:16:53 +0100

    Re-add entry__seo-01.php

--- dist/blocks/breadcrumb.php
+++
@@ -1,9 +0,0 @@
-<div id="breadcrumb" class="breadcrumb">
-	<span>
-		<span typeof="v:Breadcrumb"><a href="#" rel="v:url" property="v:title">Accueil</a></span> / 
-		<span typeof="v:Breadcrumb"><a href="#" rel="v:url" property="v:title">breadcrumb level 1</a></span> / 
-		<span typeof="v:Breadcrumb">
-			<span class="breadcrumb_last" property="v:title">breadcrumb level 2 (current)</span>
-		</span>
-	</span>
-</div>

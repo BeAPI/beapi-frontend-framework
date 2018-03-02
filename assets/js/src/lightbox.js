0fbe5a1db55dab9e095e7c318ecc8d116828ff94
Morgan Leroux <contact@morgan-leroux.com>
2018-03-02 12:08:58 +0100

    remove mfp popup

--- assets/js/src/lightbox.js
+++ assets/js/src/lightbox.js
@@ -1,34 +1,40 @@
+import $ from 'jquery'
+import 'modaal/dist/js/modaal'
+
 /**
- * Lightbox
- * http://dimsemenov.com/plugins/magnific-popup/documentation.html
+ * lightbox
  */
-
-var $ = require('jquery')
-require('../vendor/jquery.mfp')
-
-// Lightbox french translation
-$.extend(true, $.magnificPopup.defaults, {
-  tClose: 'Fermer (Esc)',
-  tLoading: 'Chargement...',
-  gallery: {
-    tPrev: 'Précédent',
-    tNext: 'Suivant',
-    tCounter: '%curr% sur %total%'
-  },
-  image: {
-    tError: '<a href="%url%">L\'image</a> ne peut pas être chargé.' // Error message when image could not be loaded
-  },
-  ajax: {
-    tError: '<a href="%url%">Le contenu</a> ne peut pas être chargé.' // Error message when ajax request failed
+const LightboxModaal = () => {
+  const self = {}
+  const opts = {
+    type: 'image'
   }
-})
 
-// lightbox in wysiwyg WP content for images AND native WP gallery
-$('.entry__content').each(function () { // the containers for all your galleries
-  $(this).find("a[href$='.png'], a[href$='.jpg'], a[href$='.gif']").magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true
-    }
-  })
-})
+  /**
+   * Initialize modaal galleries for entries links
+   * @return {[type]} [description]
+   */
+  const entryInit = () => {
+    // lightbox in wysiwyg WP content for images AND native WP gallery
+    $('.entry__content').each(function () { // the containers for all your galleries
+      // $(this)
+      $(this).find("a[href$='.png'], a[href$='.jpg'], a[href$='.gif']").attr('rel', 'gallery').modaal(opts)
+    })
+
+    console.log('Entry init')
+  }
+
+  /**
+   * [publicMethod description]
+   * @return {[type]} [description]
+   */
+  self.init = () => {
+    entryInit()
+
+    console.log('Modaal init')
+  }
+
+  return self
+}
+
+LightboxModaal().init()

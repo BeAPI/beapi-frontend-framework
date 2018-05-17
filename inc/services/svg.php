72907718accf76fae0d7748fa9cafd66523ac9f3
Maxime Culea <culea.maxime@gmail.com>
2018-05-17 16:48:11 +0200

    Revert "Move `get_the_icon()` and `the_icon`"
    This reverts commit b226076310ae2771cae99f6d0b13a78e579ffcc4.

--- inc/services/svg.php
+++ inc/services/svg.php
@@ -43,4 +43,27 @@ class SVG implements Service {
 
 		require_once( \get_theme_file_path( '/dist/assets/icons/icons.svg' ) );
 	}
+
+	/**
+	 * @param       $icon_class
+	 * @param array $additionnal_classes
+	 *
+	 * @return string
+	 */
+	public function get_the_icon( $icon_class, $additionnal_classes = array() ) {
+		$classes[] = 'icon';
+		$classes[] = sprintf( 'icon-%s', $icon_class );
+		$classes   = array_merge( $classes, $additionnal_classes );
+		$classes   = array_map('sanitize_html_class', $classes );
+
+		return sprintf( '<svg class="%s" aria-hidden="true" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-%s"></use></svg>', implode( ' ', $classes ), $icon_class );
+	}
+
+	/**
+	 * @param       $icon_class
+	 * @param array $additionnal_classes
+	 */
+	public function the_icon( $icon_class, $additionnal_classes = array() ) {
+		echo $this->get_the_icon( $icon_class, $additionnal_classes );
+	}
 }

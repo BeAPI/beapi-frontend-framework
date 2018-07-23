f17f95aa29bf3151fa6feadf7ccd5a6c3bd4d613
Nicolas Juen <njuen@beapi.fr>
2018-07-23 17:29:14 +0200

    Do not launch at 20 but 10

--- functions.php
+++ functions.php
@@ -8,6 +8,6 @@ require_once 'autoload.php';
  * Load all services
  */
 add_action( 'after_setup_theme', function () {
-	// Boot the service, at after_setup_theme
+	// Boot the service, at after_setup_theme.
 	\BEA\Theme\Framework\Framework::get_container()->boot_services();
-}, 20 );
+} );

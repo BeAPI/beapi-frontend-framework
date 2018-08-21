c5370c96d42afb2737b3073be70493dc6f9c7868
Romain Lefort <rlefort@beapi.fr>
2018-08-21 10:29:47 +0200

    Fix server.js

--- config/server.js
+++ config/server.js
@@ -1,17 +1,10 @@
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const config = require('./config')
 const webpackDev = require('./webpack.dev')
-<<<<<<< HEAD:config/server.js
-const config = require('./config')
-||||||| parent of 6213aa1... some fixes
-const svgSprite = require('./../tasks/svg-sprite')
-=======
->>>>>>> 6213aa1... some fixes:config/browser-sync.js
+const config = require('./config'))
 
 webpackDev.plugins.push(
   new BrowserSyncPlugin(
     {
-<<<<<<< HEAD:config/server.js
       proxy: 'http://[::1]:' + config.port,
       watchOptions: {
         ignoreInitial: true,
@@ -38,30 +31,6 @@ webpackDev.plugins.push(
       ],
       startPath: '/dist/index.php',
       notify: false,
-||||||| parent of 6213aa1... some fixes
-      host: '0.0.0.0',
-      port: 3001,
-      proxy: 'http://localhost:8080/',
-      files: [
-        {
-          match: ['src/**/*.svg', 'src/**/*.pug'],
-          fn: function(event, file) {
-            if (event === 'change') {
-              if (file.includes('.svg')) {
-                svgSprite()
-              }
-              const bs = require('browser-sync').get('bs-webpack-plugin')
-              setTimeout(() => {
-                bs.reload()
-              }, 3000)
-            }
-          },
-        },
-      ],
-=======
-      port: 3001,
-      proxy: 'http://localhost:' + config.devServer.port + '/',
->>>>>>> 6213aa1... some fixes:config/browser-sync.js
     },
     {
       reload: false,
@@ -70,3 +39,4 @@ webpackDev.plugins.push(
 )
 
 module.exports = webpackDev
+

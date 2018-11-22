c2f0712e9cd35b6980aed377d8c603477a79b0e2
Milan Ricoul <mricoul@beapi.fr>
2018-11-22 11:10:22 +0100

    add primary buttons

--- src/templates/00-buttons.php
+++ src/templates/00-buttons.php
@@ -3,10 +3,13 @@
 	<article class="article">
 		<div class="container">
 			<h2>Buttons</h2>
-			<a href="#" class="button">A tag</a>
-			<button type="button" class="button">Button tag</button>
+
+			<a href="#" class="button">I'm a link</a>
+			<button type="button" class="button">I'm a button</button>
 
 			<h2>Sizing</h2>
+
+			<h3>Buttons</h3>
 			<button type="button" class="button button--xs">Tiny Button</button>
 			<button type="button" class="button button--sm">Small Button</button>
 			<button type="button" class="button button">Medium Button</button>
@@ -14,18 +17,47 @@
 			<button type="button" class="button button--expand">Expanded Button</button>
 			<button type="button" class="button button--expand button--xs">Expanded tiny Button</button>
 
+			<h3>Links</h3>
+			<a href="#" class="button button--xs">Tiny Button</a>
+			<a href="#" class="button button--sm">Small Button</a>
+			<a href="#" class="button button">Medium Button</a>
+			<a href="#" class="button button--lg">Large Button</a>
+			<a href="#" class="button button--expand">Expanded Button</a>
+			<a href="#" class="button button--expand button--xs">Expanded tiny Button</a>
+
 			<h2>Colors</h2>
+			<h3>Buttons</h3>
+			<button type="button" class="button">Default Button</button>
+			<button type="button" class="button button--primary">Primary Button</button>
 			<button type="button" class="button button--error">Error Button</button>
 			<button type="button" class="button button--warning">Warning Button</button>
 			<button type="button" class="button button--success">Success Button</button>
 			<button type="button" class="button button--info">Info Button</button>
 
+			<h3>Links</h3>
+			<a href="#" class="button">Default Button</a>
+			<a href="#" class="button button--primary">Primary Button</a>
+			<a href="#" class="button button--error">Error Button</a>
+			<a href="#" class="button button--warning">Warning Button</a>
+			<a href="#" class="button button--success">Success Button</a>
+			<a href="#" class="button button--info">Info Button</a>
+
 			<h2>Outline style</h2>
+			<h3>Buttons</h3>
 			<button type="button" class="button button--outline">Outline Button</button>
+			<button type="button" class="button button--outline button--primary">Outline Primary Button</button>
 			<button type="button" class="button button--outline button--error">Outline Error Button</button>
 			<button type="button" class="button button--outline button--warning">Outline Warning Button</button>
 			<button type="button" class="button button--outline button--success">Outline Success Button</button>
 			<button type="button" class="button button--outline button--info">Outline Info Button</button>
+
+			<h3>Links</h3>
+			<a href="#" class="button button--outline">Outline Button</a>
+			<a href="#" class="button button--outline button--primary">Outline Primary Button</a>
+			<a href="#" class="button button--outline button--error">Outline Error Button</a>
+			<a href="#" class="button button--outline button--warning">Outline Warning Button</a>
+			<a href="#" class="button button--outline button--success">Outline Success Button</a>
+			<a href="#" class="button button--outline button--info">Outline Info Button</a>
 
 			<h2>Disabled style</h2>
 			<button type="button" class="button" disabled>Disabled Button</button>
@@ -35,13 +67,21 @@
 			<button type="button" class="button button--info" disabled>Disabled Info Button</button>
 
 			<h2>Border Radius style</h2>
-			<button type="button" class="button button--round">Text Style Button</button>
-			<button type="button" class="button button--round button--error">Text Style Button</button>
-			<button type="button" class="button button--round button--warning">Text Style Button</button>
-			<button type="button" class="button button--round button--success">Text Style Button</button>
-			<button type="button" class="button button--round button--info">Text Style Button</button>
+
+			<h3>Buttons</h3>
+			<button type="button" class="button button--round">Rounded Button</button>
+			<button type="button" class="button button--round button--primary">Rounded Primary Button</button>
+			<button type="button" class="button button--round button--error">Rounded Error Button</button>
+			<button type="button" class="button button--round button--warning">Rounded Warning Button</button>
+			<button type="button" class="button button--round button--success">Rounded Success Button</button>
+			<button type="button" class="button button--round button--info">Rounded Info Button</button>
 			<br />
 			<button type="button" class="button button--icon button--circle">
+				<svg class="icon icon-logo-beapi" aria-hidden="true" role="img">
+					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
+				</svg>
+			</button>
+			<button type="button" class="button button--icon button--circle button--primary">
 				<svg class="icon icon-logo-beapi" aria-hidden="true" role="img">
 					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
 				</svg>
@@ -66,6 +106,45 @@
 					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
 				</svg>
 			</button>
+
+			<h3>Links</h3>
+			<a href="#" class="button button--round">Rounded Button</a>
+			<a href="#" class="button button--round button--primary">Rounded Primary Button</a>
+			<a href="#" class="button button--round button--error">Rounded Error Button</a>
+			<a href="#" class="button button--round button--warning">Rounded Warning Button</a>
+			<a href="#" class="button button--round button--success">Rounded Success Button</a>
+			<a href="#" class="button button--round button--info">Rounded Info Button</a>
+			<br />
+			<a href="#" class="button button--icon button--circle">
+				<svg class="icon icon-logo-beapi" aria-hidden="true" role="img">
+					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
+				</svg>
+			</a>
+			<a href="#" class="button button--icon button--circle button--primary">
+				<svg class="icon icon-logo-beapi" aria-hidden="true" role="img">
+					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
+				</svg>
+			</a>
+			<a href="#" class="button button--icon button--circle button--error">
+				<svg class="icon icon-logo-beapi" aria-hidden="true" role="img">
+					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
+				</svg>
+			</a>
+			<a href="#" class="button button--icon button--circle button--warning">
+				<svg class="icon icon-logo-beapi" aria-hidden="true" role="img">
+					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
+				</svg>
+			</a>
+			<a href="#" class="button button--icon button--circle button--success">
+				<svg class="icon icon-logo-beapi" aria-hidden="true" role="img">
+					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
+				</svg>
+			</a>
+			<a href="#" class="button button--icon button--circle button--info">
+				<svg class="icon icon-logo-beapi" aria-hidden="true" role="img">
+					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-logo-beapi"></use>
+				</svg>
+			</a>
 
 			<h2>Text style</h2>
 			<button type="button" class="button button--text">Text Style Button</button>

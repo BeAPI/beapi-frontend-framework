904c45e6b4173895c854f9a2d30de2370b672272
Nicolas Fiascaro <n.fiascaro@gmail.com>
2019-09-02 11:50:32 +0200

    fix (lazyload) Chrome 76 config ready

--- src/js/app.js
+++ src/js/app.js
@@ -17,14 +17,6 @@ import './src/wrapper'
  * LazySizes configuration
  * https://github.com/aFarkas/lazysizes/#js-api---options
  */
-// lazySizes.cfg
-
-/**
- * LazySizesBgset configuration
- * https://github.com/aFarkas/lazysizes/tree/gh-pages/plugins/bgset#lazysizes-bgset-extension---responsive-background-images
- */
-lazySizes.cfg.customMedia = {}
-
 lazySizes.cfg.nativeLoading = {
-  setLoadingAttribute: true,
+  setLoadingAttribute: false,
 }

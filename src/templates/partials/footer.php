3ed3fb10f8b6641ae4221f305d7349b323b87392
Nicolas Fiascaro <n.fiascaro@gmail.com>
2019-11-04 11:53:17 +0100

    feat (build) : remove core-js in app.js and load polyfill.io bundle

--- src/templates/partials/footer.php
+++ src/templates/partials/footer.php
@@ -27,18 +27,20 @@
 		<?php require 'assets/img/icons/icons.svg'; ?>
 
 		<!-- Polyfill.io -->
-		<script crossorigin="anonymous" src="https://polyfill.io/v3/polyfill.min.js?features=NodeList.prototype.forEach%2CElement.prototype.closest"></script>
+		<script src="https://cdn.polyfill.io/v3/polyfill.min.js?features=es5,es6,fetch,Array.prototype.includes,CustomEvent,Element.prototype.closest,NodeList.prototype.forEach"></script>
+
 
 		<script>
 			// inline loadJS
 			function loadJS(e,t){"use strict";var n=window.document.getElementsByTagName("script")[0],o=window.document.createElement("script");return o.src=e,o.async=!0,n.parentNode.insertBefore(o,n),t&&"function"==typeof t&&(o.onload=t),o}
+
 			// then load your JS
 			if (sessionStorage.getItem('fonts-loaded')) {
 				// fonts cached, add class to document
-				document.documentElement.classList.remove('fonts-loading');
+				document.documentElement.classList.remove('fonts-loading')
 			} else {
 				// load script with font observing logic
-				loadJS('assets/js/vendor_async/fonts-css-async.js');
+				loadJS('assets/js/vendor_async/fonts-css-async.js')
 			}
 		</script>
 

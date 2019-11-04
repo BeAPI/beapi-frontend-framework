916747f71441d2ffa4d27b2133042bca846cc82d
Nicolas Fiascaro <n.fiascaro@gmail.com>
2019-11-04 11:53:52 +0100

    feat (wp) load polyfill in wp theme

--- inc/async/loadfont.php
+++ inc/async/loadfont.php
@@ -1,3 +1,6 @@
+<!-- Polyfill.io -->
+<script src="https://cdn.polyfill.io/v3/polyfill.min.js?features=es5,es6,fetch,Array.prototype.includes,CustomEvent,Element.prototype.closest,NodeList.prototype.forEach"></script>
+
 <script>
     // inline loadJS
     function loadJS(e,t){"use strict";var n=window.document.getElementsByTagName("script")[0],o=window.document.createElement("script");return o.src=e,o.async=!0,n.parentNode.insertBefore(o,n),t&&"function"==typeof t&&(o.onload=t),o}

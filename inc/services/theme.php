e91a98032873f063580225cdbd7a142cc05c0935
Paolo <ptesei@beapi.fr>
2019-12-03 14:04:17 +0100

    Add Gutenberg complementary styles

--- inc/services/theme.php
+++ inc/services/theme.php
@@ -54,6 +54,7 @@ class Theme implements Service {
 		add_theme_support( 'align-wide' );
 		add_theme_support( 'responsive-embeds' );
 		add_theme_support( 'editor-styles' );
+		add_theme_support( 'wp-block-styles' );
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'html5', [ 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'script', 'style' ] );
 		add_theme_support( 'title-tag' );

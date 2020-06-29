484ef681a63c85cad1f09f0c6e87dba9310fce53
Paolo <ptesei@beapi.fr>
2020-06-29 16:26:09 +0200

    fix (icons) svg footer services

--- inc/Services/Svg.php
+++ inc/Services/Svg.php
@@ -21,8 +21,6 @@ class SVG implements Service {
 	 * @param Service_Container $container
 	 */
 	public function boot( Service_Container $container ) {
-		add_action( 'wp_footer', [ $this, 'footer_icons' ] );
-		add_action( 'embed_footer', [ $this, 'footer_icons' ] );
 	}
 
 	/**
@@ -30,18 +28,6 @@ class SVG implements Service {
 	 */
 	public function get_service_name() {
 		return 'svg';
-	}
-
-	public function footer_icons() {
-		if ( ! file_exists( \get_theme_file_path( '/dist/assets/img/icons/icons.svg' ) ) ) {
-			if ( defined('WP_DEBUG') && WP_DEBUG == true ) {
-				echo '<!-- No SVG File found -->';
-			}
-
-			return;
-		}
-
-		require_once( \get_theme_file_path( '/dist/assets/img/icons/icons.svg' ) );
 	}
 
 	/**

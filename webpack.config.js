4e7a64b3fb328e7d5e2e005734e2019f9cd89493
Milan Ricoul <mricoul@beapi.fr>
2020-08-25 00:28:43 +0200

    feat (image-loader) : readd test for images but exclude svg

--- webpack.config.js
+++ webpack.config.js
@@ -166,6 +166,31 @@ module.exports = (env, argv) => {
             },
           ],
         },
+        {
+          test: /\.(png|jpe?g|gif)$/,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65,
+                },
+                pngquant: {
+                  quality: '65-90',
+                  speed: 4,
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                webp: {
+                  quality: 75,
+                },
+              },
+            },
+          ],
+        },
       ],
     },
     plugins: [

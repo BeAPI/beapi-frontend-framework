2c8cfd8dec5cddd900d367a99c31b22a891f300b
Nicolas Juen <njuen@beapi.fr>
2021-03-15 18:04:45 +0100

    Remove IE9 headers for html, remove IE9 outdated popup, update viewport from 2020 theme

--- header.php
+++ header.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html>
-<!--[if lte IE 9 ]> <html class="no-js ie lte-ie9 ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if !(IE)]><! -->
 <html class="no-js" <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
 	<script type="text/javascript">
 		//<![CDATA[
@@ -16,26 +13,11 @@
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
-	<!--[if lte IE 9]>
-		<div class="message message--outdated" id="js-ieMessage">
-			<p>
-			You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.
-			</p>
-			<p>
-			<button class="btn" id="js-ieClose">Close</button>
-			</p>
-		</div>
-		<script>
-			var ieClose = document.getElementById('js-ieClose')
-			var ieMessage = document.getElementById('js-ieMessage')
-			function ieCLosMsg() { ieMessage.style.display = 'none' }
-			ieClose.addEventListener('click', ieCLosMsg)
-		</script>
-	<![endif]-->
+<?php
+	wp_body_open();

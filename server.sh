0b02522ad0d911bf7684b5e4728805129bc0d7c8
Milan <mricoul@beapi.fr>
2021-04-22 22:25:30 +0200

    refactor : bye bye templates

--- server.sh
+++ server.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-PORT=9090
 BSPORT=3000
-
-while [ -n "$(lsof -Pi :${PORT} -sTCP:LISTEN -t)" ]; do
-    ((PORT++))
-done
 
 while [ -n "$(lsof -Pi :${BSPORT} -sTCP:LISTEN -t)" ]; do
     ((BSPORT += 2))
@@ -14,6 +9,3 @@ echo "BS will run on port ${BSPORT}"
 
 echo $PORT > ".port"
 echo $BSPORT > ".bs-port"
-
-echo "Server is running on port ${PORT} and BrowserSync on port ${BSPORT}"
-php -S localhost:${PORT}

fde794ab2f8b9a549d0af6520f4026b42c29eaaf
Milan <mricoul@beapi.fr>
2021-04-25 01:32:04 +0200

    refactor (webpack) : set common loaders in webpack.common.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const entries = require('./entries')
+const loaders = require('./loaders')
 const plugins = require('./plugins')
 
 module.exports = {
@@ -8,6 +9,9 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, '../dist'),
     publicPath: '',
+  },
+  module: {
+    rules: [loaders.FontsLoader, loaders.ImagesLoader, loaders.JSLoader],
   },
   plugins: [
     plugins.CleanWebpackPlugin,

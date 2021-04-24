8b470c927ae1079ea0b785ec6b8902e6e02c8829
Milan <mricoul@beapi.fr>
2021-04-24 21:56:33 +0200

    feat (postcss.config) : add different config for dev mode

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const loaders = require('./loaders')
 const plugins = require('./plugins')
 
 module.exports = merge(common, {
@@ -8,6 +9,9 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
+  },
+  module: {
+    rules: [loaders.FontsLoader, loaders.JSLoader, loaders.SCSSLoaderDev, loaders.SVGLoader],
   },
   plugins: [plugins.BrowserSyncPlugin, plugins.MiniCssExtractPluginDev],
 })
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,11 +1,15 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const loaders = require('./loaders')
 const plugins = require('./plugins')
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: '[name].[chunkhash:8].min.js',
+  },
+  module: {
+    rules: [loaders.FontsLoader, loaders.JSLoader, loaders.SCSSLoaderProd, loaders.SVGLoader],
   },
   plugins: [plugins.ManifestPlugin, plugins.MiniCssExtractPluginProd],
 })

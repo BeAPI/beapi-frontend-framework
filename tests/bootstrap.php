d24958ab85321ebd34fe3ee201922a3804aa79b8
Nicolas JUEN <njuen@beapi.fr>
2022-02-09 22:03:39 +0100

    Ad polyfill file for wp_parse_args

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,5 @@ WP_Mock::bootstrap();
  * Now we include any plugin files that we need to be able to run the tests. This
  * should be files that define the functions and classes you're going to test.
  */
+require_once __DIR__ . '/polyfills.php';
 require_once __DIR__ . '/../functions.php';
-
-function test_escape( $value ) {
-	return 'esc_' . $value;
-}

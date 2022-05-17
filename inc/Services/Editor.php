b05f25d7525da8a905b95ae2e56710f32d588a7e
Paolo <ptesei@beapi.fr>
2022-05-17 11:31:27 +0200

    feat (editor) add default editor styles color palettes and font sizes

--- inc/Services/Editor.php
+++ inc/Services/Editor.php
@@ -44,9 +44,18 @@ class Editor implements Service {
 		$this->style();
 
 		/**
+		 * Register custom block style
+		 */
+		$this->register_custom_block_styles();
+
+		/**
 		 * Load editor JS for ADMIN
 		 */
 		add_action( 'enqueue_block_editor_assets', [ $this, 'admin_editor_script' ] );
+		/**
+		 * White list of gutenberg blocks
+		 */
+		// add_filter( 'allowed_block_types', [ $this, 'gutenberg_blocks_allowed' ], 10, 2 );
 	}
 
 	/**
@@ -57,7 +66,77 @@ class Editor implements Service {
 	 *  - etc.
 	 *
 	 */
-	private function after_theme_setup(): void {}
+	private function after_theme_setup(): void {
+
+		//color palettes
+		add_theme_support(
+			'editor-color-palette',
+			[
+				[
+					'name'  => 'dark',
+					'slug'  => 'dark',
+					'color' => '#000000',
+				],
+				[
+					'name'  => 'light',
+					'slug'  => 'light',
+					'color' => '#ffffff',
+				],
+				[
+					'name'  => 'primary',
+					'slug'  => 'primary',
+					'color' => '#ffff00',
+				],
+				[
+					'name'  => 'secondary',
+					'slug'  => 'secondary',
+					'color' => '#00ffff',
+				],
+			]
+		);
+		// font sizes
+		add_theme_support(
+			'editor-font-sizes',
+			[
+				[
+					'name'      => 'Title 6',
+					'shortName' => 'h6',
+					'size'      => 14,
+					'slug'      => 'h6',
+				],
+				[
+					'name'      => 'Title 5',
+					'shortName' => 'h5',
+					'size'      => 16,
+					'slug'      => 'h5',
+				],
+				[
+					'name'      => 'Title 4',
+					'shortName' => 'h4',
+					'size'      => 18,
+					'slug'      => 'h4',
+				],
+				[
+					'name'      => 'Title 3',
+					'shortName' => 'h3',
+					'size'      => 24,
+					'slug'      => 'h3',
+				],
+				[
+					'name'      => 'Title 2',
+					'shortName' => 'h2',
+					'size'      => 40,
+					'slug'      => 'h2',
+				],
+				[
+					'name'      => 'Title 1',
+					'shortName' => 'h1',
+					'size'      => 58,
+					'slug'      => 'h1',
+				],
+			]
+		);
+	}
 
 	/**
 	 * editor style
@@ -116,4 +195,68 @@ class Editor implements Service {
 		);
 		$this->assets_tools->enqueue_script( 'theme-admin-editor-script' );
 	}
+
+	/**
+	 * Register custom block styles
+	 */
+
+	private function register_custom_block_styles() {
+		//button
+		register_block_style(
+			'core/button',
+			[
+				'name'  => 'reverse',
+				'label' => 'Reverse',
+			]
+		);
+		register_block_style(
+			'core/button',
+			[
+				'name'  => 'outline-reverse',
+				'label' => 'Outline reverse',
+			]
+		);
+	}
+
+	/**
+	 * Allow some core Gutenberg blocks
+	 *
+	 * @param bool|array $allowed_blocks
+	 * @param \Wp_post $post
+	 *
+	 * @return array
+	 */
+	public function gutenberg_blocks_allowed( $allowed_blocks, \Wp_post $post ): array {
+
+		$allowed = [
+			//base
+			'core/heading',
+			'core/paragraph',
+			'core/image',
+			'core/list',
+			'core/quote',
+			'core/pullquote',
+			'core/table',
+			'core/buttons',
+			'core/button',
+			'core/group',
+			'core/columns',
+			'core/column',
+			'core/media-text',
+			'core/spacer',
+			'core/separator',
+			'core/cover',
+			'core/gallery',
+			'core/video',
+			'core/file',
+			'core/embed',
+			// custom
+			'beapi/manual-block',
+			'beapi/dynamic-block',
+		];
+
+		return ( is_array( $allowed_blocks ) ) ? array_merge( $allowed, $allowed_blocks ) : $allowed;
+
+	}
+
 }

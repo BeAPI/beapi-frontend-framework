59af22032ef2f7213c4fc60da784cb164fc523cd
n-langle <nlangle@beapi.fr>
2022-05-31 17:19:58 +0200

    feat (single.php/page.php) : update markup

--- single.php
+++ single.php
@@ -1,11 +1,18 @@
-<?php get_header();
-the_post();
-?>
-	<header class="container">
-		<h1><?php the_title(); ?></h1>
-	</header>
-	<div class="blocks-container">
-		<?php the_content(); ?>;
-	</div>
 <?php
+get_header();
+
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		?>
+		<header class="container">
+			<h1><?php the_title(); ?></h1>
+		</header>
+		<div class="blocks-container">
+			<?php the_content(); ?>
+		</div>
+		<?php
+	endwhile;
+endif;
+
 get_footer();

3376dcba5be52b2cc3c4903737737384a866daf0
mricoul <mricoul@beapi.fr>
2022-11-02 09:34:06 +0100

    feat (Svg): change params in helper

--- inc/Services/Svg.php
+++ inc/Services/Svg.php
@@ -34,37 +34,36 @@ class Svg implements Service {
 
 	/**
 	 * @param string $icon_class
-	 * @param array  $settings
+	 * @param array  $additionnal_classes
 	 *
 	 * @return string
 	 */
-	public function get_the_icon( string $icon_class, array $settings = [] ): string {
+	public function get_the_icon( string $icon_class, array $additionnal_classes = [] ): string {
 		if ( empty( $icon_class ) ) {
 			return '';
 		}
 
-		if ( empty( $settings[ 'sprite_name' ] ) ) {
-			$settings[ 'sprite_name' ] = 'sprite';
-		}
+		$sprite_name = 'icons';
 
-		if ( empty( $settings[ 'additionnal_classes' ] ) ) {
-			$settings[ 'additionnal_classes' ] = [];
+		if ( false !== strpos( $icon_class, '/' ) ) {
+			$sprite_name = strtok( $icon_class, '/' );
+			$icon_class  = substr( $icon_class, strpos( $icon_class, '/' ) + 1 );
 		}
 
 		$icon_slug = strpos( $icon_class, 'icon-' ) === 0 ? $icon_class : sprintf( 'icon-%s', $icon_class );
 		$classes   = [ 'icon', $icon_slug ];
-		$classes   = array_merge( $classes, $settings[ 'additionnal_classes' ] );
+		$classes   = array_merge( $classes, $additionnal_classes );
 		$classes   = array_map( 'sanitize_html_class', $classes );
 
-		return sprintf( '<svg class="%s" aria-hidden="true" focusable="false"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="%s#%s"></use></svg>', implode( ' ', $classes ), \get_theme_file_uri( sprintf( '/dist/icons/%s.svg', $settings[ 'sprite_name' ] ) ), $icon_slug ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		return sprintf( '<svg class="%s" aria-hidden="true" focusable="false"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="%s#%s"></use></svg>', implode( ' ', $classes ), \get_theme_file_uri( sprintf( '/dist/icons/%s.svg', $sprite_name ) ), $icon_slug ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
 	/**
 	 * @param string $icon_class
-	 * @param array $settings
+	 * @param array $additionnal_classes
 	 */
-	public function the_icon( string $icon_class, array $settings = [] ): void {
-		echo $this->get_the_icon( $icon_class, $settings ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	public function the_icon( string $icon_class, array $additionnal_classes = [] ): void {
+		echo $this->get_the_icon( $icon_class, $additionnal_classes ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
 	/**

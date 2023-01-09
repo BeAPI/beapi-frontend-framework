fe49f622011a1c362999517738612aaa8dd1e6f1
Léonard Phoumpakka <lphoumpakka@beapi.fr>
2023-01-09 09:41:27 +0100

    fix ARI default image

--- inc/Services/Theme.php
+++ inc/Services/Theme.php
@@ -2,9 +2,9 @@
 
 namespace BEA\Theme\Framework\Services;
 
+use BEA\Theme\Framework\Framework;
 use BEA\Theme\Framework\Service;
 use BEA\Theme\Framework\Service_Container;
-
 
 class Theme implements Service {
 
@@ -18,6 +18,8 @@ class Theme implements Service {
 	 */
 	public function boot( Service_Container $container ): void {
 		$this->after_setup_theme();
+		add_filter( 'ari_responsive_image_default_img_path', [ $this, 'set_ari_responsive_image_default_img_path' ] );
+		add_filter( 'ari_responsive_image_default_img_name', [ $this, 'set_ari_responsive_image_default_img_name' ] );
 	}
 
 	/**
@@ -73,5 +75,73 @@ class Theme implements Service {
 	private function i18n(): void {
 		// Load theme texdomain
 		load_theme_textdomain( 'framework-textdomain', \get_theme_file_path( '/languages' ) );
+	}
+
+	/**
+	 * Set default path for ARI for minified files
+	 *
+	 * @param string $attr
+	 *
+	 * @return string
+	 *
+	 */
+	public function set_ari_responsive_image_default_img_path( string $attr ): string {
+		/**
+		 * @psalm-suppress PossiblyInvalidMethodCall
+		 * @psalm-suppress UndefinedInterfaceMethod
+		 */
+		if ( ! Framework::get_container()->get_service( 'assets' )->is_minified() ) {
+			return $attr;
+		}
+
+		return '/dist/';
+	}
+
+	/**
+	 * Set ari default image name for minified files
+	 *
+	 * @param string $default_img
+	 *
+	 * @return string
+	 *
+	 */
+	public function set_ari_responsive_image_default_img_name( string $default_img ): string {
+		/**
+		 * @psalm-suppress PossiblyInvalidMethodCall
+		 * @psalm-suppress UndefinedInterfaceMethod
+		 */
+		if ( ! Framework::get_container()->get_service( 'assets' )->is_minified() ) {
+			return $default_img;
+		}
+
+		return $this->get_min_image( $default_img );
+	}
+
+	/**
+	 * Get minified default_image
+	 *
+	 * @param string $original_image
+	 *
+	 * @return string
+	 *
+	 */
+	protected function get_min_image( string $original_image ): string {
+
+		if ( empty( $original_image ) ) {
+			return $original_image;
+		}
+
+		if ( ! file_exists( \get_theme_file_path( '/dist/assets.json' ) ) ) {
+			return $original_image;
+		}
+
+		$json   = file_get_contents( \get_theme_file_path( '/dist/assets.json' ) ); //phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+		$assets = json_decode( $json, true );
+
+		if ( empty( $assets ) || JSON_ERROR_NONE !== json_last_error() ) {
+			return $original_image;
+		}
+
+		return $assets[ 'assets/' . $original_image ] ?: $original_image;
 	}
 }

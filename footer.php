d2059cd4517d102bb5afa76468b82e43af5a4919
MarieComet <mcomet@beapi.fr>
2023-02-07 16:19:14 +0100

    fix (review) : fix  String concat is not required here; use a single string in footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		</main>
 
 		<?php
-		if ( function_exists( 'get_block_template' ) && get_block_template( get_stylesheet() . '//' . 'footer', 'wp_template_part' ) ) {
+		if ( function_exists( 'get_block_template' ) && get_block_template( get_stylesheet() . '//footer', 'wp_template_part' ) ) {
 			block_template_part( 'footer' );
 		} else {
 			get_template_part( 'components/parts/common/footer' );

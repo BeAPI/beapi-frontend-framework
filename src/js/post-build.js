9b92fb70be0ec7ec566642f9d03313adf30152af
mricoul <mricoul@beapi.fr>
2023-05-12 17:45:23 +0200

    feat (webpack): use image-minimizer-webpack-plugin

--- src/js/post-build.js
+++ src/js/post-build.js
@@ -2,11 +2,4 @@ function requireAll(r) {
   r.keys().forEach(r)
 }
 
-// SVG
-requireAll(require.context('../img/icons', true, /\.svg$/))
-
-// DEFAULT
-requireAll(require.context('../img/default', true, /\.(png|jpe?g)$/))
-
-// STATIC
-requireAll(require.context('../img/static', true, /\.(png|jpe?g|gif)$/))
+requireAll(require.context('../img', true, /\.(png|jpe?g|gif|svg)$/))

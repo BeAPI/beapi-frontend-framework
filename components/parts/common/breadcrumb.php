1acc7c1b388dcc1448812ac8f1cff951d39b003c
Milan Ricoul <mricoul@beapi.fr>
2023-10-13 09:54:35 +0200

    feat (breadcrumb): add template part

---
+++ components/parts/common/breadcrumb.php
@@ -0,0 +1,11 @@
+<?php
+if ( ! function_exists( 'yoast_breadcrumb' ) ) {
+	return;
+}
+?>
+
+<div class="breadcrumb-wrapper">
+	<div class="container">
+		<?php yoast_breadcrumb( '<div class="breadcrumb">', '</div>' ); ?>
+	</div>
+</div>

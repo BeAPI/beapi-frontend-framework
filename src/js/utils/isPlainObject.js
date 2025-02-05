acbe0f685f139b14dc7cabe35f110c3364277ec6
Nicolas Langle <nlangle@beapi.fr>
2025-02-05 17:19:41 +0100

    feat (theme): more concise isPlainObject function

--- src/js/utils/isPlainObject.js
+++ src/js/utils/isPlainObject.js
@@ -1,39 +1,9 @@
 /*!
- * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
+ * isPlainObject <https://stackoverflow.com/questions/18531624/isplainobject-thing>
  *
- * Copyright (c) 2014-2017, Jon Schlinkert.
- * Released under the MIT License.
+ * Copyright https://stackoverflow.com/users/6023279/trunow.
  */
 
-function isObject(o) {
-  return Object.prototype.toString.call(o) === '[object Object]'
-}
-
 export default function isPlainObject(o) {
-  let ctor, prot
-
-  if (isObject(o) === false) {
-    return false
-  }
-
-  // If has modified constructor
-  ctor = o.constructor
-  if (ctor === undefined) {
-    return true
-  }
-
-  // If has modified prototype
-  prot = ctor.prototype
-  if (isObject(prot) === false) {
-    return false
-  }
-
-  // If constructor does not have an Object-specific method
-  // eslint-disable-next-line no-prototype-builtins
-  if (prot.hasOwnProperty('isPrototypeOf') === false) {
-    return false
-  }
-
-  // Most likely a plain Object
-  return true
+  return o?.constructor === Object || Object.getPrototypeOf(o ?? 0) === null
 }

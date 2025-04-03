2dc071b405a084cfedc127ae6607d2ec6d1d6cb4
Cédric Andrietti <candrietti@beapi.fr>
2025-04-03 11:13:59 +0200

    Fix accessibility for image block with legend

---
+++ src/js/classes/ImageBlock.js
@@ -0,0 +1,31 @@
+import AbstractDomElement from './AbstractDomElement'
+
+// ----
+// class
+// ----
+class ImageBlock extends AbstractDomElement {
+  constructor(element, options) {
+    const instance = super(element, options)
+
+    // avoid double init :
+    if (!instance.isNewInstance()) {
+      return instance
+    }
+
+    const el = this._element
+    const figure = el.closest('.wp-block-image')
+
+    figure.setAttribute('role', 'group')
+    figure.setAttribute('aria-label', el.textContent)
+  }
+}
+
+// ----
+// init
+// ----
+ImageBlock.init('.wp-block-image figcaption')
+
+// ----
+// export
+// ----
+export default ImageBlock

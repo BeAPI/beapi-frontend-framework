91500e428e4a118a75b8d5f95a0e519532d3a39f
Cédric Andrietti <candrietti@beapi.fr>
2026-05-07 16:33:23 +0200

    feat(header): add "Go to header" skip link for improved navigation accessibility

--- header.php
+++ header.php
@@ -27,6 +27,9 @@ use BEA\Theme\Framework\Helpers\Custom_Menu_Walker;
 	<?php wp_body_open(); ?>
 	<nav class="skip-links skip-links--hidden" aria-label="<?php esc_attr_e( 'Fast access links', 'beapi-frontend-framework' ); ?>">
 		<ul>
+			<li>
+				<a href="#header"><?php esc_html_e( 'Go to header', 'beapi-frontend-framework' ); ?></a>
+			</li>
 			<li class="is-desktop-only">
 				<a href="#menu"><?php esc_html_e( 'Go to main navigation menu', 'beapi-frontend-framework' ); ?></a>
 			</li>
